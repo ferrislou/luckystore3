a83388faa92182106480984c44baa27ea573218a
ferris <ferrislou@gmail.com>
2023-02-09 17:47:22 +0800

    navbar added

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -106,12 +106,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 # LANGUAGE_CODE = 'en-us'
-LANGUAGE_CODE = 'zh-hant'
+# LANGUAGE_CODE = 'zh-hant'
+LANGUAGE_CODE = 'zh-hans'
 
 LANGUAGES = [
     ('en', 'English'), 
@@ -131,11 +131,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') #! added by flou
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'ecommerce/static') #! register all static folders
 ]
-
+print('*** STATIC_ROOT:', STATIC_ROOT)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
