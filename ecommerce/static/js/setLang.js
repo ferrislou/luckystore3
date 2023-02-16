0c59c36572e310f997a5af9accab15c6520714ff
ferris <ferrislou@gmail.com>
2023-02-16 16:30:01 +0800

    changing lang fuction3

--- ecommerce/static/js/setLang.js
+++ ecommerce/static/js/setLang.js
@@ -1,57 +1,20 @@
 
-		document.getElementById("lang_selector").onchange = langChangeListener;
+document.getElementById("lang_selector").onchange = langChangeListener;
 		
-		function langChangeListener()
-		{
-			var value = this.value
-			const langs = ['en_us', 'zh-hans', 'zh-hant'];
+const langs = ['en_us', 'zh-hans', 'zh-hant'];
 
-			if (langs.includes(value)){
-				changeLang(value);
-			}
-		}
-		function changeLang(lang) {
-				console.log("Language selected", lang);
-			}
+function langChangeListener()
+{
+    var value = this.value
 
-
-language = localStorage.getItem("language")
-if (language === null) {
-    localStorage.setItem("language", "en");
-} else {
-    console.log("preferred language:", language)
-    if (language === "en_us") {
-        console.log("preferred language:", language);
-    } else if (language === "zh-hans") {
-        console.log("preferred language:", language);
-    } else if (language === "zh-hant") {
-        console.log("preferred language:", language);
+    if (langs.includes(value)){
+        localStorage.setItem("language", value);
+        changeLang(value);
+        // language = localStorage.getItem("language")
     }
 }
-
+function changeLang(lang) {
+    console.log("Language selected", lang);
+}
 // localStorage.clear();
 console.log("localStorage end")
-
-window.setInterval(changeText, 1000);
-var currentdate = new Date(); 
-var datetime = "Last Sync: " + currentdate.getDate() + "/"
-                + (currentdate.getMonth()+1)  + "/" 
-                + currentdate.getFullYear() + " @ "  
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();
-console.log('datetime:', datetime)
-
-function changeText(){
-    var currentdate = new Date(); 
-    var date = currentdate.getDate() + "/"
-                    + (currentdate.getMonth()+1)  + "/" 
-                    + currentdate.getFullYear()  
-    var time = currentdate.getHours() + ":"  
-                    + currentdate.getMinutes() + ":" 
-                    + currentdate.getSeconds();
-    ele_returned = document.getElementById('datetime')
-    if (ele_returned !== null){
-        ele_returned.innerHTML = 'Date:' + date + ' Time:' + time;
-    }
-}
