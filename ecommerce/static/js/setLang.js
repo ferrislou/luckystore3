0541fa841f5c6baac4ebed10f2f57411f15e965a
ferris <ferrislou@gmail.com>
2023-02-20 14:33:18 +0800

    b4 final modification

--- ecommerce/static/js/setLang.js
+++ ecommerce/static/js/setLang.js
@@ -1,10 +1,9 @@
-
 ele_returned = document.getElementById("lang_selector");
 if (ele_returned !== null){
     ele_returned.onchange = langChangeListener;
 }
 		
-const langs = ['en_us', 'zh-hans', 'zh-hant'];
+const langs = ['en', 'zh'];
 
 function clearStorage(){
     language = localStorage.getItem("language")
@@ -19,10 +18,42 @@ function langChangeListener()
 
     if (langs.includes(value)){
         localStorage.setItem("language", value);
+        changeLang_peter(value)
         changeLang(value);
     }
 }
 function changeLang(lang) {
     console.log("Language selected 999", lang);
 }
-clearStorage()
+
+var langList = ['en', 'zh'];
+
+function hideLang(lang) {
+    var langElems = document.querySelectorAll('.' + lang)
+    langElems.forEach((elem) => {
+      elem.style.display = "none"
+    })
+}
+
+function changeLang_peter(lang) {
+    langList.forEach((langEle) => {
+      // if language matches, display
+      if (langEle == lang) {
+        var langElems = document.querySelectorAll('.' + langEle)
+        langElems.forEach((elem) => {
+          elem.style.display = "block"
+        })
+      }
+      // hide the language text
+      else {
+        hideLang(langEle)
+      }
+    })
+}
+
+language = localStorage.getItem("language")
+if (language !== null){
+    changeLang_peter(language)
+    console.log('peter')
+}
+
--- store/models.py
+++ store/models.py
@@ -13,6 +13,7 @@ class Customer(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
+    name_zh = models.CharField(max_length=200, null=False)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     digital = models.BooleanField(default=False, null=True, blank=False)
     image_main = models.ImageField(null=True, blank=True)
